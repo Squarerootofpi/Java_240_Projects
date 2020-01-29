2a0da19607e7ecb8bd21007e22afd0e8ac4f8e51
Squarerootofpi <squarerootsofpi@gmail.com>
2020-01-29 06:37:51 -0700

    Finished Evil Hangman (With an evil smile)

--- EvilHangman/src/hangman/EvilHangman.java
+++ EvilHangman/src/hangman/EvilHangman.java
@@ -2,6 +2,9 @@ package hangman;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 public class EvilHangman {
 
@@ -20,12 +23,118 @@ public class EvilHangman {
         File dictionaryFile = new File(dictionary);
         int wordLength = Integer.parseInt(args[3]);
         int guesses = Integer.parseInt(args[4]);
-        //Make an evilhangmangame instance
+        String currentWord;
+        //Make an evilhangman game instance
         EvilHangmanGame game = new EvilHangmanGame();
+        StringBuilder letterSB = new StringBuilder();
         //Do the start game function (loop functionality will be inside that!
         try {
             game.startGame(dictionaryFile,wordLength);
-            System.out.println("finished startgameTry");
+            game.setUnderscoredWord(wordLength);
+            Scanner io = new Scanner(System.in);
+            //SortedSet<Character> guessedLetters;
+            for (int i = guesses; i > 0; i--) {
+                /**
+                 * You have 1 guess left
+                 * Used letters: a e f i l m o s t u
+                 * Word: -u---
+                 * Enter guess: b
+                 * You lose!
+                 * The word was: vuggy
+                 */
+                game.getGuessedLetters();
+                letterSB.delete(0, letterSB.length());
+                System.out.println("You have " + String.valueOf(i) + " guess left");
+                for (Character c: game.getGuessedLetters()
+                     ) {
+                    if (letterSB.length() == 0)
+                    {
+                        letterSB.append(c);
+                    }
+                    else {
+                        letterSB.append(" " + String.valueOf(c));
+                    }
+                }
+                currentWord = game.getShownWord();
+                System.out.println("Used letters: " + letterSB.toString());
+                System.out.println("Word: " + currentWord);
+                System.out.println("Enter guess: ");
+                String read = io.next();
+
+                /**
+                 * Prompt the user for his/her next letter guess. If the guess is not an upperor lower-case letter then print “Invalid input” on the next line and reprompt.
+                 * If the user has already guessed the letter print “You already used that
+                 * letter” on the next line and reprompt.
+                 */
+                try {
+
+                    if (read.length() > 1 || read.length() == 0)
+                    {
+                        System.out.println("Invalid input");
+                        ++i; //Do this so we get the right amount of guesses still
+                        continue;
+                    }
+                    Character input = read.charAt(0);
+                    if (!Character.isAlphabetic(input))
+                    {
+                        System.out.println("Invalid input");
+                        ++i; //Do this so we get the right amount of guesses still
+                        continue;
+                    }
+                    //finally....
+                    game.makeGuess(input);
+                    //If we reach this point, we know that it was a valid input.
+                    /**
+                     * Report the number of position(s) in which the letter appears. If the current
+                     * word list doesn't contain any word with that letter, decrement the number
+                     * of remaining guesses and print “Sorry, there are no <inputLetter>’s” on a
+                     * new line (replace inputLetter with the actual letter input). If all words in the
+                     * list contain at least one word with that letter, print “Yes, there is <number>
+                     * <letter>” where letter is the letter guessed and numbers is the number of
+                     * times letter appears in the word with the fewest number of those letters.
+                     */
+                    if (game.getLettersAdded()>0)
+                    {
+                        System.out.println("Yes, there is " + game.getLettersAdded() + " " + input);
+                    }
+                    else {
+                        System.out.println("Sorry, there are no " + input + "’s");
+                    }
+                    //check if they won:
+                    if (game.didWin())
+                    {
+                        System.out.println("You Win!");
+                    }
+                    else {
+                        System.out.println("");
+                    }
+                    /**
+                     * if they win: “You Win!” and display the correct word.
+                     *
+                     * You have 7 guesses left
+                     * Used letters: a e i
+                     * Word: -----
+                     * Enter guess: o
+                     * Sorry, there are no o's
+                     *
+                     * You have 6 guesses left
+                     * Used letters: a e i o
+                     * Word: -----
+                     * Enter guess: u
+                     * Yes, there is 1 u
+                     */
+                } catch (GuessAlreadyMadeException ex)
+                {
+                    System.out.println("You already used that letter");
+                    ++i; //Do this so we get the right amount of guesses still
+                    continue;
+                }
+
+            }
+            if (!game.didWin()) {
+                System.out.println("You lose!");
+            }
+            System.out.println("The word was: " + game.getAWord());
         } catch (IOException e) {
             e.printStackTrace();
 
--- EvilHangman/src/hangman/EvilHangmanGame.java
+++ EvilHangman/src/hangman/EvilHangmanGame.java
@@ -8,12 +8,15 @@ import java.util.*;
 public class EvilHangmanGame implements IEvilHangmanGame {
     private SortedSet<Character> guessedLetters;
     private TreeSet<String> dictionary;
-    private StringBuilder shownWord;
+    private String shownWord;
+    private Integer lettersAdded;
 
     public EvilHangmanGame()
     {
         guessedLetters = new TreeSet<Character>();
         dictionary = new TreeSet<String>();
+        shownWord = "";
+        lettersAdded = 0;
     }
 
 
@@ -58,23 +61,9 @@ public class EvilHangmanGame implements IEvilHangmanGame {
         }
         containsOne = null;
 
-        playGame();
-
 
     }
-    private void playGame()
-    {
-        /**
-         * You have 1 guess left
-         * Used letters: a e f i l m o s t u
-         * Word: -u---
-         * Enter guess: b
-         * You lose!
-         * The word was: vuggy
-         */
-        System.out.println();
 
-    }
     @Override
     public Set<String> makeGuess(char guess) throws GuessAlreadyMadeException {
         char finGuess = Character.toLowerCase(guess);
@@ -112,6 +101,7 @@ public class EvilHangmanGame implements IEvilHangmanGame {
         //Get greatest partition:
         //ArrayList<Integer> pvals = new ArrayList<Integer>();
         TreeSet<String> returnSet = new TreeSet<String>();
+        String partitionKey = null;
         Integer max = -1;
         for (Map.Entry<String, TreeSet<String>> partition: partitions.entrySet()
              ) {
@@ -119,17 +109,121 @@ public class EvilHangmanGame implements IEvilHangmanGame {
             {
                 max = partition.getValue().size();
                 returnSet = partition.getValue();
+                partitionKey = partition.getKey();
+            }
+            else if (max == partition.getValue().size())
+            {
+                partitionKey = tieBreaker(partitionKey, partition.getKey(), finGuess);
+                returnSet = partitions.get(partitionKey);
             }
         }
         dictionary = returnSet; //Get rid of old dictionary.
+        setShownWord(mergeUnderscoreStr(partitionKey));
         partitions = null; //Don't need them anymore.
         return returnSet;
     }
 
+    public Boolean didWin()
+    {
+        if (shownWord.indexOf('-') == -1) { return true; }
+        return false;
+    }
+
+    public String getAWord()
+    {
+        return dictionary.first();
+    }
+
+    /**\
+     * @param s2
+     * @return
+     */
+    private String mergeUnderscoreStr(String s2)
+    {
+        lettersAdded = 0;
+        if (shownWord.length() != s2.length())
+        {
+            return "Error merging input strings";
+        }
+        StringBuilder sb = new StringBuilder(shownWord);
+        for (int i = 0; i < s2.length(); i++) {
+            Character c = s2.charAt(i);
+            if (!c.equals('-')) //If it's not an underscore, merge that char with s2
+            {
+                sb.replace(i, i+1,String.valueOf(c));
+                lettersAdded++;
+            }
+        }
+        return sb.toString();
+    }
+
+    public Integer getLettersAdded() {
+        return lettersAdded;
+    }
+
+    //This breaks ties according to specs.
+    private String tieBreaker(String s1, String s2, char guess)
+    {
+        /**
+         * Choose the group in which the letter does not appear at all.
+         * 2. If each group has the guessed letter, choose the one with the
+         * fewest letters.
+         * 3. If this still has not resolved the issue, choose the one with the
+         * rightmost guessed letter.
+         * 4. If there is still more than one group, choose the one with the next
+         * rightmost letter. Repeat this step (step 4) until a group is
+         * chosen.
+         */
+        Boolean s1b = s1.contains(String.valueOf(guess));
+        Boolean s2b = s2.contains(String.valueOf(guess));
+        if (s1b && !s2b) { return s2; }
+        else if (!s1b && s2b) {return s1;}
+        else {
+            //We know they both have guess's char in them.
+            //Iterate backwards, with rightmost char!
+            ArrayList<Integer> s1Set = new ArrayList<Integer>();
+            ArrayList<Integer> s2Set = new ArrayList<Integer>();
+            for (int i = s1.length() - 1; i > -1; i--) {
+                if (s1.charAt(i) == guess) { s1Set.add(i); }
+                if (s2.charAt(i) == guess) { s2Set.add(i); }
+            }
+            if (s1Set.size() == s2Set.size()) //Size is the same, Do the rightmost one then! :)
+            {
+                //Go through the backward list set
+                for (int i = 0; i < s1Set.size(); i++) {
+                    if (!s1Set.get(i).equals(s2Set.get(i))) //if they aren't the same index
+                    {
+                        if (s1Set.get(i) > s2Set.get(i)) //If the last index is bigger than this one.
+                        {
+                            return s1;
+                        }
+                        else {return s2; }
+                    }
+                }
+            }
+            //We know that the sets have different amounts: so,
+            else if (s1Set.size() > s2Set.size()) { return s2; }
+            else { return s1; }
+        }
+        return "";
+    }
     @Override
     public SortedSet<Character> getGuessedLetters() {
         return guessedLetters;
     }
 
+    public String getShownWord() {
+        return shownWord;
+    }
+    public void setUnderscoredWord(int wordLength) {
+        char[] charArray = new char[wordLength];
+        Arrays.fill(charArray, '-');
+        this.shownWord = new String(charArray);
+
+        this.shownWord = shownWord;
+    }
+    public void setShownWord(String shownWord) {
+        this.shownWord = shownWord;
+    }
     //Drop dictionary
 }
